ee8ca062b95562a787710978ad2c10fac6ac0a1a
Andrew White <aeewhite@gmail.com>
2016-08-16 20:40:49 -0500

    Initial Commit

---
+++ todayview/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  todayview
+//
+//  Created by Andrew White on 8/16/16.
+//  Copyright © 2016 Andrew White. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
