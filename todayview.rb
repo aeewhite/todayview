a9004ba7cfb219cdb3ebbde1d3389a16ea37a1de
Andrew White <aeewhite@gmail.com>
2016-08-18 10:10:12 -0500

    Update brew hash for 0.4

--- todayview.rb
+++ todayview.rb
@@ -1,8 +1,8 @@
 class Todayview < Formula
   desc "View the days events from OS X System Calendar"
   homepage "https://github.com/aeewhite/todayview"
-  url "https://github.com/aeewhite/todayview/archive/0.2.tar.gz"
-  sha256 "9d12fb2efac90dca2ab24ad9e9f5c4988f74a868a5d705d32c9fa3fd4015a24f"
+  url "https://github.com/aeewhite/todayview/archive/0.4.tar.gz"
+  sha256 "7b1a9d03bb06d433c0a1cbb9f20244add520f6960f6ea5490dbb852ebb7ada9a"
   head "https://github.com/aeewhite/todayview.git"
 
   depends_on :xcode => :build
